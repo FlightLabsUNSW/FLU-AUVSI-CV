519c7afc0effce05fd06d884d4424df98bffe3c1
flu <flightlabsunsw@gmail.com>
2019-06-12 03:05:59 +1000

    data manu update

--- image_labeling/genobjdetect_data_manu.py
+++ image_labeling/genobjdetect_data_manu.py
@@ -17,7 +17,7 @@ class Formatter(Enum):
 data_format = Formatter.yolo_dataset
 empty_imgs_dir = 'empty_imgs'
 flu_odlcs_dir = 'flu_odlcs'
-out_dir = 'labeled_dataset'
+out_dir = 'genobjdet_labeled_dataset'
 cwd = os.getcwd()
 empty_imgs = os.listdir(os.path.join(cwd, empty_imgs_dir))
 flu_odlcs = sorted(os.listdir(os.path.join(cwd, flu_odlcs_dir)))
@@ -169,12 +169,12 @@ for img in empty_imgs:
             cv.destroyAllWindows()
 
         if data_format == Formatter.yolo_dataset:
-            ins_string += str(0) + ','
+            ins_string += str(20) + ' '
             for key,val in curr_odlc_shape_loc.items():
-                ins_string += str(val[0]/img_arr.shape[0]) + ',' 
-                ins_string += str(val[1]/img_arr.shape[1]) + ','
-            ins_string += str(odlc['yolo_spot']['width']/img_arr.shape[0]) + ','
-            ins_string += str(odlc['yolo_spot']['height']/img_arr.shape[1]) + ','
+                ins_string += str(val[0]/img_arr.shape[0]) + ' '
+                ins_string += str(val[1]/img_arr.shape[1]) + ' '
+            ins_string += str(odlc['yolo_spot']['width']/img_arr.shape[0]) + ' '
+            ins_string += str(odlc['yolo_spot']['height']/img_arr.shape[1]) + ' '
             ins_string += '\n'
             if DEBUG: print(str(img_odlc_num)+'.txt', ins_string, end='')
             
